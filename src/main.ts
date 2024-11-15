3f1455280461571ad7c08afff3c8cac386ee1a61
SogoHlopec <sogo246@gmail.com>
2024-11-15 05:04:01 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
+import * as swaggerUi from 'swagger-ui-express';
+import * as yaml from 'js-yaml';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 dotenv.config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerDocument = yaml.load(
+    readFileSync(join(__dirname, '..', 'doc', 'api.yaml'), 'utf8'),
+  );
+
+  app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
   await app.listen(PORT);
 }
 bootstrap();
